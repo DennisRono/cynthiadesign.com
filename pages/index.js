a51a4fa26ce8d7db7bf1eca5a96a6cac8867a619
Finn Neron <72130813+DennisRono@users.noreply.github.com>
2021-04-10 21:23:36 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
