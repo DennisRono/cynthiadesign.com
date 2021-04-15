2fbeeaa245fa31052b38a701466b328b5869c0cd
Finn Neron <72130813+DennisRono@users.noreply.github.com>
2021-04-15 20:32:14 +0300

    Add files via upload

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,17 +3,35 @@ import styles from '../styles/login.module.css'
 
 export const login = () => {
     return (
-        <div className={styles.login}>
-           <div className={styles.logincontainer}>
-                <div className={styles.loginbox}>
-                    <h3>login</h3>
-                    <div className={styles.signin}>
-                        <input type="text" placeholder="Username"/>
-                        <input type="password" placeholder="password"/>
-                        <input type="submit" value="login"/>
-                    </div>
+        <div className={styles.main}>
+            <div className={styles.container}>
+                <div className={styles.signupcontent}>
+                    <form className={styles.form}>
+                        <h2>Login</h2>
+                        <div className={styles.inputcontainer}>
+                            <input type="text" className={styles.forminput} placeholder="Username"/>
+                        </div>
+                        <div className={styles.inputcontainer}>
+                            <input type="password" className={styles.forminput} placeholder="Password"/>
+                        </div>
+                        <div className={styles.inputcontainer}>
+                            <input type="checkbox"/>
+                            <label htmlFor="" className={styles.labelagree}>
+                                I agree all <a href="#">Terms & Service</a>
+                            </label>
+                        </div>
+                        <div className={styles.inputcontainer}>
+                            <input type="submit" value="Sign Up" className={styles.formsubmit}/>
+                        </div>
+                    </form>
+                    <p className={styles.loginhere}>
+                        Don't have an account? 
+                        <a href="#"> Register</a><br/>
+                        <span className={styles.backhome}><a href="/">Home</a></span>
+                    </p>
+                    
                 </div>
-           </div>
+            </div>
         </div>
     )
 }
