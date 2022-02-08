9d4517f6372b617ebe4d9012fc3698be60280b41
unknown <finnneron1@gmail.com>
2022-02-08 10:10:31 +0300

    globals stylings

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import styles from '../styles/footer.module.css'
-import TwitterIcon from '@material-ui/icons/Twitter';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import InstagramIcon from '@material-ui/icons/Instagram';
 
 export const Footer = () => {
     return (
-        <div></div>
+        <div>
+            
+        </div>
     )
 }
 
