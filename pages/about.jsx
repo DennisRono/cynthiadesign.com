c504a5984668bf8dbc40760fcc144c256177f877
unknown <finnneron1@gmail.com>
2022-02-08 10:03:25 +0300

    cleanup

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import styles from '../styles/about.module.css'
 
 export const about = () => {
     return (
         <div>
-            <Navbar />
-            <div className={styles.aboutpage}>
-                <h1>About</h1>
-            </div>
-            <Footer />
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
-import Head from 'next/head'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import Slim from '../components/Slim'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.main}>
+    <div>
       
     </div>
   )
