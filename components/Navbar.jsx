a881fd93e6b8c2e88d43b76bdbf8babcdf1d9692
unknown <finnneron1@gmail.com>
2022-02-08 09:56:47 +0300

    cleaning for rebuild

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,68 +1,8 @@
 import React from 'react'
-import styles from '../styles/navbar.module.css'
-import TwitterIcon from '@material-ui/icons/Twitter';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import YouTubeIcon from '@material-ui/icons/YouTube';
-import PhoneIcon from '@material-ui/icons/Phone';
-import EmailIcon from '@material-ui/icons/Email';
-import HorizontalSplitRoundedIcon from '@material-ui/icons/HorizontalSplitRounded';
-
 
 export const Navbar = () => {
     return (
-        <div className={styles.jumbotron} id="jumbotron">
-            <div className={styles.backimage}>
-            <div className={styles.conbusi}>
-                <div className={styles.conbusiContainer}>
-                    <p>Welcome to cynthia design</p>
-                    <div className={styles.conbusiRight}>
-                        <div className={styles.conbusiEmail}>
-                            <i><EmailIcon className={styles.EmailIcon}/></i>
-                        </div>
-                        <div className={styles.conbusiNumber}>
-                            <i><PhoneIcon /></i>
-                        </div>
-                        <div className={styles.conbusiSocials}>
-                            <TwitterIcon />
-                            <FacebookIcon />
-                            <YouTubeIcon />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.constructo}>
-                <div className={styles.constructoContainer}>
-                    <div className={styles.constructoLogo}>
-                        <h1>Chela</h1>
-                    </div>
-                    <div className={styles.constructoButtons}>
-                        <a className={styles.register} href="/register">Sign up</a>
-                        <a className={styles.login} href="/login">Sign in</a>
-                    </div>
-                    <div className={styles.responsivebars}>
-                    <HorizontalSplitRoundedIcon />
-                    </div>
-                </div>
-            </div>
-            <div className={styles.navbar}>
-                <nav>
-                    <ul className={styles.navbarul}>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/services">Services</a></li>
-                        <li><a href="/contact">Contact Us</a></li>
-                    </ul>
-                </nav>
-                <div className={styles.navSearch}>
-                    <input type="text" className={styles.navInput} placeholder="Search..."/>
-                    <input type="submit" value="Search" className={styles.navbutton}/>
-                </div>
-            </div>
-            </div>
-            {/* <div className={styles.distro}>
-                yoh
-            </div> */}
-            
+        <div>
         </div>
     )
 }
--- components/Slim.jsx
+++ components/Slim.jsx
@@ -1,61 +1,8 @@
 import React from 'react'
-import styles from '../styles/slim.module.css'
 
 export const Slim = () => {
     return (
-        <div className={styles.slim}>
-            <div className={styles.slimcontainer}>
-                <span>schedule</span>
-                <h2>Downloads</h2>
-                <p>All of our special downloads</p>
-            <div className={styles.navlinkwrap}>
-	            <div className={styles.navpills} role="tablist" aria-orientation="vertical">
-	              <a className={styles.navlink} data-toggle="pill" href="#v-pills-8" role="tab" aria-controls="v-pills-8" aria-selected="true">
-	              Monday <span>Fitness Program</span></a>
-
-	              <a className={styles.navlink} data-toggle="pill" href="#v-pills-9" role="tab" aria-controls="v-pills-9" aria-selected="false">
-	              Tuesday <span>Crossfit Program</span></a>
-
-	              <a className={styles.navlink} data-toggle="pill" href="#v-pills-10" role="tab" aria-controls="v-pills-10" aria-selected="false">
-	              Wednesday <span>Aerobic Program</span></a>
-
-	              <a className={styles.navlink} data-toggle="pill" href="#v-pills-11" role="tab" aria-controls="v-pills-11" aria-selected="false">
-	              Thursday <span>Yoga Classes</span></a>
-
-	              <a className={styles.navlink} data-toggle="pill" href="#v-pills-12" role="tab" aria-controls="v-pills-12" aria-selected="false">
-	              Friday <span>Fitness Program</span></a>
-
-	              <a className={styles.navlink} data-toggle="pill" href="#v-pills-13" role="tab" aria-controls="v-pills-13" aria-selected="false">
-	              Saturday <span>Yoga Classes</span></a>
-
-	              <a className={styles.navlink} data-toggle="pill" href="#v-pills-14" role="tab" aria-controls="v-pills-14" aria-selected="false">
-	              Sunday <span>Crossfit Program</span></a>
-
-	            </div>
-                <div className={styles.slimcontentarea}>
-                    <div className={styles.slimrightone}>
-					<div className={styles.slimdownloadone}>
-						<button>&times;</button>
-					</div>
-					<div className={styles.slimdownloadtwo}></div>
-					<div className={styles.slimdownloadthree}></div>
-					<div className={styles.slimdownloadfour}></div>
-                    </div>
-                    <div className={styles.slimrighttwo}>
-					<div className={styles.slimdownloadfive}></div>
-					<div className={styles.slimdownloadsix}></div>
-					<div className={styles.slimdownloadseven}></div>
-					<div className={styles.slimdownloadeight}></div>
-					</div>
-                    <div className={styles.slimrightthree}>
-					<div className={styles.slimdownloadnine}></div>
-					<div className={styles.slimdownloadten}></div>
-					<div className={styles.slimdownloadeleven}></div>
-					<div className={styles.slimdownloadtwelve}></div>
-					</div>
-                </div>
-	          </div>
-            </div>
+        <div>
         </div>
     )
 }
